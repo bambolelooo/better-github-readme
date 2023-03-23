81aab6ed73e22ae11c0f88b19d1e8987597a6540
Ilya Evlanov <evlanov.ilya@gmail.com>
2023-03-23 18:10:59 -0400

    added configs for themes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import {ConfigProvider} from "antd";
+import * as darkThemeConfig from "./dark_theme.json"
+import * as lightThemeConfig from "./light_theme.json"
+import {useState} from "react";
 
 function App() {
+    const [darkTheme, setDarkTheme] = useState(true)
   return (
-      <ConfigProvider>
+      <ConfigProvider theme={darkTheme ? darkThemeConfig : lightThemeConfig}>
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
