f3885b725e2194d3a5623444ace60ba249eb133b
Dobby <apple@Dobbyde-MacBook-Pro.local>
2023-03-25 15:38:03 -0400

    move .env to root and add auth to github at server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const { ApolloServer } = require('apollo-server-express')
 const path = require('path')
@@ -5,7 +6,12 @@ const path = require('path')
 const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection')
 
-require('dotenv').config()
+const cors = require('cors')
+const fetch = (...args) =>
+    import('node-fetch').then(({ default: fetch }) => fetch(...args))
+
+const client_id = process.env.GITHUB_CLIENT_ID
+const client_secret = process.env.GITHUB_CLIENT_SECRET
 
 const PORT = process.env.PORT || 3001
 const app = express()
@@ -16,6 +22,7 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')))
@@ -23,6 +30,48 @@ if (process.env.NODE_ENV === 'production') {
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
+//code being sent from front end
+app.get('/getAccessToken', async (req, res) => {
+    const code = req.query.code
+    console.log(code)
+    const params =
+        '?client_id=' +
+        client_id +
+        '&client_secret=' +
+        client_secret +
+        '&code=' +
+        code
+
+    // fetch access token from github
+    const response = await fetch(
+        'https://github.com/login/oauth/access_token' + params,
+        {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+            },
+        }
+    )
+    const data = await response.json()
+    console.log(data)
+    res.json(data)
+})
+
+//get user info from github
+app.get('/getUserInfo', async (req, res) => {
+    const access_token = req.query.access_token
+    console.log(access_token)
+    const response = await fetch('https://api.github.com/user', {
+        method: 'GET',
+        headers: {
+            Authorization: 'token ' + access_token,
+        },
+    })
+    const data = await response.json()
+    console.log(data)
+    res.json(data)
 })
 
 const startApolloServer = async (typeDefs, resolvers) => {
