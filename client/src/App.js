54afcd347afdcb3b2b45b66a5647b68d0f9796fd
elainebautista0919 <elaine.bautista@gmail.com>
2023-03-25 14:36:07 -0400

    added home route and link

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import {ConfigProvider} from "antd";
 import * as darkThemeConfig from "./dark_theme.json"
 import * as lightThemeConfig from "./light_theme.json"
 import {useState} from "react";
 
+import Home from './pages/Home';
+
 function App() {
     const [darkTheme, setDarkTheme] = useState(true)
   return (
       <ConfigProvider theme={darkTheme ? darkThemeConfig : lightThemeConfig}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+
+        <BrowserRouter>
+          <Routes>
+          <Route path='/' element={<Home />}></Route>
+          </Routes>
+        
+        </BrowserRouter>
+
       </ConfigProvider>
 
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import '../App.css';
 
 function Home() {
     return (
             <section className='home-page'>
                 
                 <h1>Better GitHub ReadMe</h1>
+                <h2>Welcome to Better Github ReadMe or BGR for short!</h2>
 
                 <div className='home-container'>
                     
                     <h3>What we do?</h3>
-                    <ul>
+                    <ul className='home-ul'>
                         <li>
                             BGR is an application that allows developers the ability to write and edit ReadMe files and push them directly to Github
                         </li>
                     </ul>
 
                     <h3>How does it work?</h3>
-                    <ul>
+                    <ul className='home-ul'>
                         <li>
                             Simply click the Login button on the top right which will redirect you to login to your Github account
                         </li>
