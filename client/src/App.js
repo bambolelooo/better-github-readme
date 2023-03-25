05d5fb67dcdc36a6525a7c1a1233f0d12f764068
elainebautista0919 <elaine.bautista@gmail.com>
2023-03-25 15:03:33 -0400

    added nav and footer

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,22 @@ import {ConfigProvider} from "antd";
 import * as darkThemeConfig from "./dark_theme.json"
 import * as lightThemeConfig from "./light_theme.json"
 import {useState} from "react";
-
+import Nav from './pages/Nav';
 import Home from './pages/Home';
+import Footer from './pages/Footer';
 
 function App() {
     const [darkTheme, setDarkTheme] = useState(true)
   return (
       <ConfigProvider theme={darkTheme ? darkThemeConfig : lightThemeConfig}>
-
-        <BrowserRouter>
-          <Routes>
+          
+      <BrowserRouter>
+        <Nav />
+        <Routes>
           <Route path='/' element={<Home />}></Route>
-          </Routes>
-        
-        </BrowserRouter>
-
+        </Routes>
+      </BrowserRouter>
+      <Footer />
       </ConfigProvider>
 
   );
