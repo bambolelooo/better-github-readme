06616607d9ab8f05ba47c18fa2b006d03dd24475
Dobby <apple@Dobbyde-MacBook-Pro.local>
2023-03-27 19:19:13 -0400

    add resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,31 @@
+const { User, Readme, Snippet, Template } = require('../models')
+const resolvers = {
+    Query: {
+        user: async () => {
+            return User.findOne({}).populate('repos')
+        },
+        getReadme: async (parent, { repoId }) => {
+            return Readme.findOne({ _id: repoId })
+        },
+        getSnippets: async (parent, { snippetId }) => {
+            return Snippet.findOne({ _id: snippetId }).populate(
+                'snippetContent'
+            )
+        },
+        getTemplates: async (parent, { templateId }) => {
+            return Template.findOne({ _id: templateId }).populate(
+                'templateContent'
+            )
+        },
+    },
+    Mutation: {
+        SaveReadme: async (parent, { repoId, readmeContent }) => {
+            const readme = await Readme.findOneAndUpdate(
+                { _id: repoId },
+                { readmeContent },
+                { new: true }
+            )
+            return readme
+        },
+    },
+}
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,52 +1,52 @@
-const { gql } = require("apollo-server-express");
+const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
-  type User {
-    _id: ID
-    githubUsername: String
-    email: String
-    repos: [Repo]
-  }
+    type User {
+        _id: ID
+        githubUsername: String
+        email: String
+        repos: [Repo]
+    }
 
-  type Repo {
-    _id: ID
-    repoName: String
-    repoLink: String
-  }
+    type Repo {
+        _id: ID
+        repoName: String
+        repoLink: String
+    }
 
-  type Readme {
-    _id: ID
-    repoId: ID
-    readmeContent: String
-  }
+    type Readme {
+        _id: ID
+        repoId: ID
+        readmeContent: String
+    }
 
-  type Snippet {
-    _id: ID
-    snippetName: String
-    snippetContent: String
-  }
+    type Snippet {
+        _id: ID
+        snippetName: String
+        snippetContent: String
+    }
 
-  type Template {
-    _id: ID
-    templateName: String
-    templateContent: String
-  }
+    type Template {
+        _id: ID
+        templateName: String
+        templateContent: String
+    }
 
-  type Auth {
-    token: ID!
-    user: User
-  }
+    type Auth {
+        token: ID!
+        user: User
+    }
 
-  type Query {
-    me: User
-    getReadme(repoId: ID!): Readme
-    getSnippets: [Snippet]
-    getTemplates: [Template]
-  }
+    type Query {
+        user: User
+        getReadme(repoId: ID!): Readme
+        getSnippets: [Snippet]
+        getTemplates: [Template]
+    }
 
-  type Mutation {
-    SaveReadme(repoId: ID!, readmeContent: String!): Readme
-  }
-`;
+    type Mutation {
+        SaveReadme(repoId: ID!, readmeContent: String!): Readme
+    }
+`
 
-module.exports = typeDefs;
+module.exports = typeDefs
