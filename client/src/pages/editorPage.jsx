30a9d6172224ee4240e3c1f458619e8a3bba976c
Ilya Evlanov <evlanov.ilya@gmail.com>
2023-03-27 18:29:27 -0400

    mvp editor page

--- client/src/pages/editorPage.jsx
+++ client/src/pages/editorPage.jsx
@@ -1,4 +1,54 @@
 import { Button } from 'antd'
+import { useState, useRef } from 'react'
+import styles from '../css/editorPage.module.css'
+import TextEditor from '../components/TextEditor'
+
 export default function EditorPage(props) {
-    return <main></main>
+    const { darkTheme } = props
+    const [textareaValue, setTextareaValue] = useState('')
+    const textareaRef = useRef(null)
+    function handleSnippet(snippet) {
+        const textarea = textareaRef.current
+        const pos = textarea.selectionEnd
+        const currentValue = textarea.value
+        const newValue =
+            currentValue.substring(0, pos) + '\n\n' + snippet + '\n'
+        currentValue.substring(pos)
+        setTextareaValue(newValue)
+        setTimeout(() => {
+            textarea.setSelectionRange(
+                pos + snippet.length + 2,
+                pos + snippet.length + 2
+            )
+            textarea.focus()
+        }, 0)
+    }
+    const snippets = [
+        { name: 'Description', value: '## Description' },
+        { name: 'Installation', value: '## Installation' },
+    ]
+
+    return (
+        <main className={styles.main}>
+            <section className={styles.snippetSection}>
+                <h3>Snippets</h3>
+                {snippets.map((snippet, index) => (
+                    <Button
+                        type={'primary'}
+                        key={index}
+                        onClick={() => handleSnippet(snippet.value)}
+                    >
+                        {snippet.name}
+                    </Button>
+                ))}
+            </section>
+            <section className={styles.textSection}>
+                <TextEditor
+                    textareaValue={textareaValue}
+                    setTextareaValue={setTextareaValue}
+                    textareaRef={textareaRef}
+                />
+            </section>
+        </main>
+    )
 }
