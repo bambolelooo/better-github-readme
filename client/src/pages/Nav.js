88359ef860a3bb7862b25efe07fe411a0d1bbdc0
Ilya Evlanov <evlanov.ilya@gmail.com>
2023-03-27 22:09:16 -0400

    added cun and moon icons

--- client/src/pages/Nav.js
+++ client/src/pages/Nav.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import '../App.css'
-
-const Nav = ({ darkTheme }) => {
+import { Switch } from 'antd'
+import { BsMoonFill } from 'react-icons/bs'
+import { FaSun } from 'react-icons/fa'
+const Nav = ({ darkTheme, handleClick }) => {
     const auth = localStorage.getItem('user')
     const navigate = useNavigate()
     const logout = () => {
@@ -19,6 +21,20 @@ const Nav = ({ darkTheme }) => {
                         ? require('../resources/logodark512.png')
                         : require('../resources/logolight512.png')
                 }
+            />
+            <Switch
+                defaultChecked={false}
+                checkedChildren={
+                    <div
+                        style={{
+                            marginTop: '2px',
+                        }}
+                    >
+                        <BsMoonFill />
+                    </div>
+                }
+                unCheckedChildren={<FaSun />}
+                onClick={handleClick}
             />
             {auth ? (
                 <ul className="nav-ul nav-right">
