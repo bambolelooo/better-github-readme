da2a26014e9be0c679f2cfbf719230409d1bed52
Ilya Evlanov <evlanov.ilya@gmail.com>
2023-03-27 21:24:28 -0400

    set up dark theme

--- client/src/App.js
+++ client/src/App.js
@@ -2,44 +2,71 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 // import { ConfigProvider } from 'antd'
-import { ConfigProvider, theme, Switch, Card } from "antd";
+import { ConfigProvider, theme, Switch, Card, Layout } from 'antd'
 import * as darkThemeConfig from './dark_theme.json'
 import * as lightThemeConfig from './light_theme.json'
 import { useState } from 'react'
 import Nav from './pages/Nav'
 import Home from './pages/Home'
-import Footer from './pages/Footer'
 import EditorPage from './pages/EditorPage'
+import { Content, Header } from 'antd/es/layout/layout'
 
 function App() {
-    // const [darkTheme, setDarkTheme] = useState(false)
-    const { defaultAlgorithm, darkAlgorithm } = theme;
-    const [isDarkMode, setIsDarkMode] = useState(false);
-    const [darkTheme, setDarkTheme] = useState(false);
+    const { defaultAlgorithm, darkAlgorithm } = theme
+    console.log(darkAlgorithm)
+    const [darkTheme, setDarkTheme] = useState(false)
 
     const handleClick = () => {
-      setIsDarkMode((previousValue) => !previousValue);
-    };
+        setDarkTheme(!darkTheme)
+    }
 
     return (
-        // <ConfigProvider theme={darkTheme ? darkThemeConfig : lightThemeConfig}>
-        <ConfigProvider theme={{ algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,}}>
-        <Card >
-          <Switch defaultChecked={false} checkedChildren='Dark' unCheckedChildren='Light' onClick={handleClick} >
-          </Switch>
-            <BrowserRouter>
-                <Nav />
-                <Routes>
-                    <Route path="/" element={<Home />}></Route>
-                    <Route
-                        path="/editor"
-                        element={<EditorPage darkTheme={darkTheme} />}
-                    ></Route>
-                </Routes>
-            </BrowserRouter>
-            <Footer />
-        </Card>
-        </ConfigProvider>
+        <div style={{ height: '100%' }}>
+            <ConfigProvider
+                theme={
+                    darkTheme
+                        ? {
+                              ...darkThemeConfig,
+                              algorithm: darkAlgorithm,
+                          }
+                        : {
+                              ...lightThemeConfig,
+                              algorithm: defaultAlgorithm,
+                          }
+                }
+            >
+                <div
+                    style={{
+                        color: `${darkTheme ? '#F7EDCF' : '#484F58'}`,
+                        height: '100%',
+                    }}
+                >
+                    <Layout>
+                        <Switch
+                            defaultChecked={false}
+                            checkedChildren="Dark"
+                            unCheckedChildren="Light"
+                            onClick={handleClick}
+                        ></Switch>
+                        <BrowserRouter>
+                            <Nav />
+
+                            <Content>
+                                <Routes>
+                                    <Route path="/" element={<Home />}></Route>
+                                    <Route
+                                        path="/editor"
+                                        element={
+                                            <EditorPage darkTheme={darkTheme} />
+                                        }
+                                    ></Route>
+                                </Routes>
+                            </Content>
+                        </BrowserRouter>
+                    </Layout>
+                </div>
+            </ConfigProvider>
+        </div>
     )
 }
 
